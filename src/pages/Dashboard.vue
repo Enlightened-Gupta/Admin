eb8eed2735ab2dd87ccc0d96b0a46e43ebc8b556
Shivani K <shivanik.bbi18@rvce.edu.in>
2022-01-19 16:42:34 +0530

    changes on dashboard.vue

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page class="q-pa-sm">
+
+    <card-social icon_position="left"/>
+
+    <card-charts/>
+
+    <div class="row q-col-gutter-sm  q-py-sm">
+      <tab-social/>
+      <card-with-image/>
+    </div>
+
+    <div class="row q-col-gutter-sm  q-pb-sm">
+      <todo-list/>
+
+      <card-time-line/>
+    </div>
+
+    <table-visits/>
+  </q-page>
+</template>
+
+<script>
+import {defineComponent,defineAsyncComponent} from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: {
+    CardSocial: defineAsyncComponent(() => import('components/cards/CardSocial')),
+    CardCharts: defineAsyncComponent(() => import('components/cards/CardCharts')),
+    CardWithImage: defineAsyncComponent(() => import('components/cards/CardWithImage')),
+  },
+})
+</script>
